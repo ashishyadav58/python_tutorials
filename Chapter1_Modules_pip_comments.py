5d257513cf3de03bcf55eda022a5adb86a6e2db1
Ashish Yadav <technicaldynomite@gmail.com>
2023-09-06 14:15:11 +0530

    updated

---
+++ Chapter1_Modules_pip_comments.py
@@ -0,0 +1,20 @@
+import math
+import random
+import requests
+
+square = math.sqrt(9)
+
+print(square)
+
+# There are two types of comments in python
+
+# 1. Single line comments - (#)
+# 2. multiline Comments - (''' or """)
+
+
+
+# url = "https://www.example.com"  #"" or ''
+
+# get = requests.get(url)
+
+# print(get)
