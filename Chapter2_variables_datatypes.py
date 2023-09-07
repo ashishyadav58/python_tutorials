553e7e64c030ba00ecf40503ae6b8895eef06378
Ashish Yadav <technicaldynomite@gmail.com>
2023-09-07 19:47:03 +0530

    UPDATED

---
+++ Chapter2_variables_datatypes.py
@@ -0,0 +1,84 @@
+# a = 54
+# b = "rohan"
+
+# REserved KEywords In Python
+
+# Identifiers:- class, function, variablers
+
+
+# RULES FOR DEFINING A VARIABLE NAME
+# 1. Variable name can contain alphabets, digits and Symbols
+# 2. Variable name can only start with an alphabet and underscore
+# 3. Variable name can't start with a digit
+
+# _yot = 463
+# print(_yot)
+
+# Rate5 = "sell"
+
+# print(Rate5)
+
+# print(type(b)) # type() is a function to check the datatype of variable
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+# OPERATORS IN PYTHON
+
+# 1. Arithmetic Operators: +, -, *, /
+# 2. Assignment operators: =, +=, -=, etc
+# 3. Comparison Operators: ==, >, >=, <, != etc
+# 4. Logical Operators: and , or, not
+
+
+# EXAMPLE:-
+# sum = 4
+# sum += 6
+# print(sum)
+
+# if not sum == 4:
+#     print("yes")
+# else:
+#     print("no")
+
+
+# <PYTHON IS A CASE SENSITIVE>
+
+# a = 54
+# A = 54
+
+# print(a, A)
+
+
+# <CONVERSION OF DATATYPES IN PYTHON>
+
+# str()
+# float()
+# int()
+
+# disk = "rohan"
+# print(type(int(disk)))
+
+
+# print(type(disk))
+
+# new = float(disk)
+
+# print(type(new))
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+sum = a + b 
+
+print(sum)
+
+
+
+# PRACTICE PROBLEM
+
+# 1. TAKE USER INPUT AND PERFORM ARITHMETIC OPERATIONS.
+
+# 2. CONVERT THE DATA TYPE OF END RESULT OF FIRST PROGRAM.
