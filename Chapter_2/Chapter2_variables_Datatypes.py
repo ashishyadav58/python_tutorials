97f1f1f8760ea70899eb607b2962404491439ad3
Ashish Yadav <technicaldynomite@gmail.com>
2024-01-05 13:24:36 +0530

    updated

---
+++ Chapter_2/Chapter2_variables_Datatypes.py
@@ -0,0 +1,29 @@
+########################## VARIABLES ###########################
+
+a = 5 # 'a' is  a variable whereas 5 is the value assigned to a.
+
+# OTHER EXAMPLES:-
+b = "Rohan"
+c = 44.65
+
+########################## DATATYPES ###########################
+
+print(type(a), type(b), type(c))  # 'TYPE' is a function used to indentify datatype of variable.
+
+name = "rohan" # 'str' - String Datatype
+age = 34 # 'int' - Integer Datatype
+income = 4400.54 # 'float' - Floating Point Numbers (Decimal Numbers)
+married = True # 'boolean' - Boolean Datatype
+
+my_list = ["car", 54, 42.5, False] # list Datatype mutable
+
+my_tuple = (1, "apple", "banana", "orange", 55) # Immutable Datatype (Can't change the value of it)
+
+my_set = {1, 2, 3, 4, 5} # Set containing Integers
+
+my_dict = {
+    "Rohan": 33,
+    "Suhani": 76,
+    "raju": 56,
+    "mohan": 86
+} # Dictionary is a group of key - value pairs.
