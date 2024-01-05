a8f44ccbc5873378e90cad4951f215c90a777b1b
Ashish Yadav <technicaldynomite@gmail.com>
2024-01-05 21:48:15 +0530

    updated

--- Chapter_3/Chapter3_Operators.py
+++ Chapter_3/Chapter3_Operators.py
@@ -1,1 +1,16 @@
-print(2**3**2)
+a = 2 + 3 * 5 # Check precedence to solve this equation
+
+b = 5 * 6 / 7 # This equation has same precedence check for Associativity
+
+d = 33
+d /= 3
+
+print(d)
+print(type(d))
+
+# Python is a case sensitive
+
+a = 34
+A = 30
+
+print(a, A)
