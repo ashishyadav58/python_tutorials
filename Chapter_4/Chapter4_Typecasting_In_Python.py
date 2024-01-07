9add60e64b1d9a924e4bf719b0abdb28a068f7f1
Ashish Yadav <technicaldynomite@gmail.com>
2024-01-07 15:21:19 +0530

    updated

--- Chapter_4/Chapter4_Typecasting_In_Python.py
+++ Chapter_4/Chapter4_Typecasting_In_Python.py
@@ -1,3 +1,9 @@
+a = "40"
+b = 3
+
+c = a + b
+print(c)
+
 # Implicit Typecasting
 a = 4.6
 b = 5
@@ -7,3 +13,18 @@ c = a + b
 # Here Python automatically converts data types of variable 'b' from 'int' to 'float' so that it will do addition of floats
 
 print(c)
+
+# Explicit Typecasting
+a = "4"
+b = 5
+
+c = int(a) + b
+
+# Here we change the datatype of variable 'a' so that we can do addtion of both numbers.
+
+print(c)
+
+# In this case we can't convert the datatype of variable d because it is a text not numeric
+d = "Rohan"
+
+print(int(d))
